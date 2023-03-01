d37b855a8d03880626a1c2345b7dc865ff8b092e
marcArisen <markjingnum9@gmail.com>
2023-03-01 13:07:49 +0700

    setup

--- example.js
+++ example.js
@@ -1,12 +1,51 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require('socket.io');
+// const io = new Server(server);
+// // const cors = require('cors');
+// // app.use(cors())
+
+// // app.get('/', (req, res) => {
+// //   res.sendFile(__dirname + '/example.html');
+// // });
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+//   socket.on('chat message', (msg) => {
+//     console.log('chat message: ' + msg);
+//     io.emit('chat message', msg);
+//   });
+// });
+
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
+
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origins: ['http://localhost:5173']
+  }
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/example.html');
+  res.send('<h1>Hey Socket.io</h1>');
 });
 
 io.on('connection', (socket) => {
@@ -14,12 +53,8 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('chat message', (msg) => {
-    console.log('chat message: ' + msg);
-    io.emit('chat message', msg);
-  });
 });
 
-server.listen(3000, () => {
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
---
+++ kuy-gift/src/App.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div>
+  <!-- <ilisten></ilisten> -->
+  <p1>KUY</p1>
+</div>
+</template>
+
+<script>
+import SocketioService from './services/socketio.service.js';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  created(){
+    SocketioService.setupSocketConnection();
+  },
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
